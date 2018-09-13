9c36f4ef8be90efccced0aa3bfec3b5f1cbc5dcc
Nannings <mrnannings@gmail.com>
2018-09-13 15:14:49 +0200

    UILevel;

---
+++ Assets/Scripts/UILevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILevel : MonoBehaviour
+{
+    public Level level;
+    public Text levelIDText;
+    public GameObject lockImage;
+
+    private Transform startParent;
+    private Image[] stars;
+
+    private void Awake()
+    {
+        startParent = transform.Find("Stars").transform;
+        stars = startParent.GetComponentsInChildren<Image>();
+    }
+
+    public void SetStars(int stars)
+    {
+        for (int i = 0; i < stars; i++)
+        {
+            this.stars[i].color = Color.white;
+            Debug.Log("White: " + i);
+        }
+    }
+}
